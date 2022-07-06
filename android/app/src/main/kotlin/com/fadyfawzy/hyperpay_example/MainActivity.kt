33842edfc16b6f9e0015b788caf9ff3b62a2c6f5
Fady Fawzy <FadyFawzy.Official@gmail.com>
2022-07-06 11:59:53 +0200

    Create MethodChannel between the Android Platform and Flutter

--- android/app/src/main/kotlin/com/fadyfawzy/hyperpay_example/MainActivity.kt
+++ android/app/src/main/kotlin/com/fadyfawzy/hyperpay_example/MainActivity.kt
@@ -1,6 +1,16 @@
 package com.fadyfawzy.hyperpay_example
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.fadyfawzy/paymentMethod"
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            // This method is invoked on the main thread.
+            // TODO
+        }
+    }
 }
