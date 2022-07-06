db58d2f4a4aab371772bf137cdceb344eb63abdd
Fady Fawzy <FadyFawzy.Official@gmail.com>
2022-07-06 12:01:40 +0200

    Configure the Checkout Settings

--- android/app/src/main/kotlin/com/fadyfawzy/hyperpay_example/MainActivity.kt
+++ android/app/src/main/kotlin/com/fadyfawzy/hyperpay_example/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.fadyfawzy.hyperpay_example
 
+import com.oppwa.mobile.connect.checkout.meta.CheckoutSettings
+import com.oppwa.mobile.connect.provider.Connect
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
@@ -12,5 +15,14 @@ class MainActivity: FlutterActivity() {
             // This method is invoked on the main thread.
             // TODO
         }
+    }
+
+    fun receivePayment(call: MethodCall){
+        val paymentBrands = hashSetOf("VISA", "MASTER", "DIRECTDEBIT_SEPA")
+
+        val checkoutSetting = CheckoutSettings(call.arguments("checkoutId"), paymentBrands, Connect.ProviderMode.TEST)
+
+        // Set shopper result URL
+        checkoutSetting.setShopperResultUrl("om.fadyfawzy.hyperpay_example.payments://result")
     }
 }
